de8a827884fd0abf94b878ac690b5c5af351405e
COPELABS <copelabs@COPELABSs-MacBook-Pro.local>
2018-02-27 08:54:29 +0000

    MVP Conversation Activity

---
+++ app/src/main/java/pt/ulusofona/copelabs/oi_ndn/interfaces/ConversationContract.java
@@ -0,0 +1,8 @@
+package pt.ulusofona.copelabs.oi_ndn.interfaces;
+
+/**
+ * Created by copelabs on 26/02/2018.
+ */
+
+public interface ConversationContract {
+}
---
+++ app/src/main/java/pt/ulusofona/copelabs/oi_ndn/presenters/ConversationPresenter.java
@@ -0,0 +1,8 @@
+package pt.ulusofona.copelabs.oi_ndn.presenters;
+
+/**
+ * Created by copelabs on 26/02/2018.
+ */
+
+public class ConversationPresenter {
+}
